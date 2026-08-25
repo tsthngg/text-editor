5544adf27ec494af2010b925cb08db8b5d3a30cb
tsthngg <tathung140@gmail.com>
2026-08-25 16:59:01 +0700

    feat: add basic rendering & complete cursor display

--- tedt.c
+++ tedt.c
@@ -20,53 +20,57 @@ typedef struct {
 } Buffer;
 
 typedef struct {
-    int position;
+    int pos;
 } Cursor;
 
 int buffer_init(Buffer *buffer);
 void buffer_backspace(Buffer *buffer, int pos);
 void buffer_print(Buffer *buffer);
 void buffer_insert(Buffer *buffer, int pos, char c);
+void cursor_to_screen(HANDLE hConsole, Buffer *buffer, Cursor *cursor);
+void editor_render(HANDLE hConsole, Buffer *buffer, Cursor *cursor);
 
 int main() {
-    printf("Tedt\n---------------------------------------------------------\n");
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    
     Buffer buffer; 
     if (!buffer_init(&buffer)) return 1;
+    
     Cursor cursor;
-    cursor.position = buffer.length;
+    cursor.pos = buffer.length;
+    cursor_to_screen(hConsole, &buffer, &cursor);
+
     while(1) {
         int c = _getch();
         if (c == ESC) break;
-        if (c == BACKSPACE) {
-            buffer_backspace(&buffer, cursor.position);
+        else if (c == BACKSPACE) {
+            buffer_backspace(&buffer, cursor.pos);
             printf("\b \b");
-            if (cursor.position != 0) cursor.position--;
-            continue;
+            if (cursor.pos != 0) cursor.pos--;
         }
-        if (c == ENTER) {
-            buffer_insert(&buffer, cursor.position, '\n');
-            cursor.position++;
+        else if (c == ENTER) {
+            buffer_insert(&buffer, cursor.pos, '\n');
+            cursor.pos++;
             printf("\n");
-            continue;
         }
-        if (c == SPECIAL) {
+        else if (c == SPECIAL) {
             int key = _getch();
             if (key == LEFT) {
-                if (cursor.position == 0) continue;
-                cursor.position--;
-                continue;
+                if (cursor.pos == 0) continue;
+                cursor.pos--;
+                cursor_to_screen(hConsole, &buffer, &cursor);
             }
             if (key == RIGHT) {
-                if (cursor.position == buffer.length) continue;
-                cursor.position++;
-                continue;
+                if (cursor.pos == buffer.length) continue;
+                cursor.pos++;
+                cursor_to_screen(hConsole, &buffer, &cursor);
             }
-            continue;
         }
-
-        buffer_insert(&buffer, cursor.position, c);
-        cursor.position++;
-        printf("%c", c);
+        else {
+            buffer_insert(&buffer, cursor.pos, c);
+            cursor.pos++;
+        }
+        editor_render(hConsole, &buffer, &cursor);
     } 
 
     buffer_print(&buffer);
@@ -86,13 +90,12 @@ int buffer_init(Buffer *buffer) {
 }
 
 void buffer_backspace(Buffer *buffer, int pos) {
-    if (buffer->length == 0) return;
+    if (pos == 0 || buffer->length == 0) return;
     memmove(&buffer->data[pos-1], &buffer->data[pos], buffer->length - pos + 1);
     buffer->length--;
 }
 
 void buffer_print(Buffer *buffer) {
-    printf("\nBuffer:\n");
     for (int i = 0; i < buffer->length; i++) {
         printf("%c", buffer->data[i]);
     }
@@ -112,4 +115,25 @@ void buffer_insert(Buffer *buffer, int pos, char c) {
     memmove(&buffer->data[pos + 1], &buffer->data[pos], buffer->length - pos + 1);
     buffer->data[pos] = c;
     buffer->length++;
+}
+
+void cursor_to_screen(HANDLE hConsole, Buffer *buffer, Cursor *cursor) {
+    int x = 0, y = 0;
+    for (int i = 0; i < cursor->pos; i++) {
+        if (buffer->data[i] == '\n') {
+            y++;
+            x = 0;
+        }
+        else x++;
+    }
+    COORD pos;
+    pos.X = x;
+    pos.Y = y;
+    SetConsoleCursorPosition(hConsole, pos);
+}
+
+void editor_render(HANDLE hConsole, Buffer *buffer, Cursor *cursor) {
+    system("cls");
+    buffer_print(buffer);
+    cursor_to_screen(hConsole, buffer, cursor);
 }
